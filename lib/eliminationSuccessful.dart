26a4b18d188e97151948da5f861de9526e2eb27f
jldeyoung <jldeyoung@live.com>
2020-03-18 14:31:24 -0700

    Attempted addition of MongoDB support.

---
+++ lib/eliminationSuccessful.dart
@@ -0,0 +1,17 @@
+///*Returns a boolean whether or not the elimination was successful (based on a check of the
+///*information entered against the answers in the database).
+///*Revision 0: returns true under all circumstances.
+///*Future structure included as comments and pseudocode.
+
+
+//class heading here
+bool eliminationSuccessful() {
+  /*
+    Choose a question randomly
+    Call a method that displays a prompt to the user [askUser(questionID)]
+        Get the answer to that question
+    Compare the strings(ignore case)
+        Return string equality
+   */
+  return true;//VERSION 0 ONLY!!!
+}
