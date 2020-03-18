992cc1e8fa45a6772f716a5e1433ec774d432a2c
jldeyoung <jldeyoung@live.com>
2020-03-18 16:25:47 -0700

    Added eliminationSuccessful.dart

--- lib/eliminationSuccessful.dart
+++ lib/eliminationSuccessful.dart
@@ -7,7 +7,8 @@
 //class heading here
 bool eliminationSuccessful() {
   /*
-    Choose a question randomly
+    Random number generator between [1,# of icebreaker questions]
+    Pass that as the question/answer we want to retrieve
     Call a method that displays a prompt to the user [askUser(questionID)]
         Get the answer to that question
     Compare the strings(ignore case)
