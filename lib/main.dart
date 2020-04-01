211e584f0ac4982dd2e1ee58dd6f6abae1f74b1a
Zachary Haroian <zaharoian@willamette.edu>
2020-04-01 16:46:53 -0700

    Added TODOs for mockup

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,186 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AssassinApp());
 
-class MyApp extends StatelessWidget {
+//  TODO: Variables to be replaced with Controller
+class Controller {
+  static var cardinalRedSwatch = MaterialColor(0xFF791716, _cardinalColorMap);
+  static var primarySwatch = cardinalRedSwatch;
+  static const cardinalRed = Color(0xFF791716);
+  static const primaryColor = cardinalRed;
+  static const title = 'Assassin';
+  static const versionDate = 'April 2020';
+  static const versionCode = "1.0";
+  static const homePageTitle = 'Assassin';
+  static const homeBottomTabTitle = 'Home';
+  static const homeBottomTabIcon = Icon(Icons.home);
+  static const leaderboardBottomTabTitle = 'Leaderboard';
+  static const leaderboardBottomTabIcon = Icon(Icons.format_list_numbered);
+  static const feedBottomTabTitle = 'Feed';
+  static const feedBottomTabIcon = Icon(Icons.format_align_justify);
+  static const settingsBottomTabTitle = 'Settings';
+  static const settingsBottomTabIcon = Icon(Icons.settings);
+  static var username = "John Smith";
+
+  static Map<int, Color> _cardinalColorMap =
+  {
+    50: Color.fromRGBO(121, 23, 22, .1),
+    100: Color.fromRGBO(121, 23, 22, .2),
+    200: Color.fromRGBO(121, 23, 22, .3),
+    300: Color.fromRGBO(121, 23, 22, .4),
+    400: Color.fromRGBO(121, 23, 22, .5),
+    500: Color.fromRGBO(121, 23, 22, .6),
+    600: Color.fromRGBO(121, 23, 22, .7),
+    700: Color.fromRGBO(121, 23, 22, .8),
+    800: Color.fromRGBO(121, 23, 22, .9),
+    900: Color.fromRGBO(121, 23, 22, 1.0),
+  };
+
+  static Map<int, String> _gamelist = {
+    0:"Game 1",
+    1:"Game 2",
+    2:"Game 3",
+    3:"Game 4",
+    4:"Game 5",
+    5:"Game 6"
+  };
+
+  static getGameList() {
+    return _gamelist;
+  }
+}
+
+
+class AssassinApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: Controller.title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Controller.primarySwatch,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AssassinHomePage(title: Controller.homePageTitle),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class AssassinHomePage extends StatefulWidget {
+  //  Constructor
+  AssassinHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AssassinHomePageState createState() => _AssassinHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AssassinHomePageState extends State<AssassinHomePage> {
+  //  Current selected widget
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  //  List of possible widgets
+  static const List<Widget> _widgetOptions = <Widget>[
+    // TODO: @Sam - get mockup
+    Text('Index 0: Home'),
+    //  TODO: @Zach - get mockup
+    Text('Index 1: Leaderboard'),
+    //  TODO: @David - get mockup
+    Text('Index 2: Feed'),
+    Text('Index 3: Settings')
+  ];
+
+  //  Change the current widget
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
+  // Called on setState
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem> [
+            // Home
+            BottomNavigationBarItem(
+              icon: Controller.homeBottomTabIcon,
+              title: Text(Controller.homeBottomTabTitle),
+            ),
+
+            //  Leaderboard
+            BottomNavigationBarItem(
+              icon: Controller.leaderboardBottomTabIcon,
+              title: Text(Controller.leaderboardBottomTabTitle)
+            ),
+
+            //  Feed
+            BottomNavigationBarItem(
+              icon: Controller.feedBottomTabIcon,
+              title: Text(Controller.feedBottomTabTitle)
+            ),
+
+            //  TODO: Move Settings to Drawer
+            BottomNavigationBarItem(
+              icon: Controller.settingsBottomTabIcon,
+              title: Text(Controller.settingsBottomTabTitle)
+            )
+          ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black54,
+        showUnselectedLabels: true,
+        onTap: _onItemTapped,
+      ),
+      drawer: Drawer(
+        child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: Controller.getGameList().length + 3,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Controller.primaryColor
+                ),
+                child: Text(
+                  Controller.username,
+                  style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 24
+                  )
+                ),
+              );
+            }
+
+            if (index == Controller.getGameList().length + 1){
+              return const Divider();
+            }
+
+            if (index == Controller.getGameList().length + 2) {
+              return AboutListTile(
+                icon: Icon(Icons.info),
+                applicationIcon: FlutterLogo(),
+                applicationName: Controller.title,
+                applicationVersion: Controller.versionCode,
+                applicationLegalese: Controller.versionDate,
+                dense: true,
+              );
+            }
+            return ListTile(
+              leading: Icon(Icons.accessibility),
+              title: Text(Controller.getGameList()[index - 1])
+            );
+          },
+        )
+      )
     );
   }
 }
