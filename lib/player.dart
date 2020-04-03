d902565a805e8a438ef49c94fe753ddeb62fa1d4
jldeyoung <jldeyoung@live.com>
2020-04-03 16:31:06 -0600

    implemented several methods in player.dart (the Player class).

--- lib/player.dart
+++ lib/player.dart
@@ -10,6 +10,7 @@ class Player {
   String username;
   bool isEliminated;
   bool isTargeted;
+  int score;
 
   Player(pID,tID,rn,un,isE,isT)
   {
@@ -19,6 +20,43 @@ class Player {
     this.username = un;
     this.isEliminated = isE;
     this.isTargeted = isT;
+    score = 0;
   }
 
+  ///THIS GETS CALLED BY THE GUI BUTTON.
+  ///Member of class Player.
+  ///It gets a question/answer pair from the remote database, makes a call to the GUI to have the user
+  /// answer, and compares the user's answer to their target's answer in the database.
+  /// Returns true and calls this.eliminatePlayer() if the answers are the same, false otherwise.
+  bool eliminationSuccessful() {
+    int questionID; //= Random number generator between [1,# of icebreaker questions];
+    String question;// = getFromDB(questionID);//Get the question with that random ID from remote DB
+    String answer;// = getFromDB(the answer to question);
+    askUser(question);
+    if(identical(askUser(question),(answer))){
+        eliminateTarget();
+        return true;
+    }
+    return false;
+  }
+
+//TODO: Player.updatePlayerScore() and Player.updateTarget().
+
+  ///member of class Player
+  ///Sets getFromDB(this.targetID).isEliminated to true
+  ///Updates this.score
+  ///Gets this player a new target.
+  void eliminateTarget() {
+    updatePlayerScore();
+    updateTarget();
+    //get this.targetID from remote DB
+    //set the target's isEliminated=true;
+  }
+
+  ///Does GUI things. @GUI team please implement
+  String askUser(String question){
+    //shows a prompt to the user with @param question and a text box to answer.
+    //@return the answer inputted to text box.
+    return "";
+  }
 }
