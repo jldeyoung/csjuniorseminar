eb045ba22d7f6a7cacffce3b3b6c68c0faaaf608
jldeyoung <jldeyoung@live.com>
2020-04-13 15:27:10 -0600

    Created LeaderboardItem class and shell getLeaderboard() method.

--- lib/LeaderboardItem.dart
+++ lib/LeaderboardItem.dart
@@ -1,15 +1,5 @@
-import 'package:csjuniorseminar/player.dart';
-
 class LeaderboardItem{
   int position;
   String name;
   int points;
-  Player localPlayer;
-
-  LeaderboardItem(){
-    //localPlayer = [local Player object];
-    //position = ???;
-    name = localPlayer.getPlayerName();
-    points = localPlayer.getPlayerScore();
-  }
 }
