05e09cd9d4c1beb0ec0c093027cf1c99b947cf4d
jldeyoung <jldeyoung@live.com>
2020-04-22 17:04:40 -0600

    Added documentation and clarifying comments.

--- lib/Game.dart
+++ lib/Game.dart
@@ -2,17 +2,24 @@
 import 'package:csjuniorseminar/LeaderboardItem.dart';
 import 'package:csjuniorseminar/player.dart';
 
+///The Game class represents a single instance of a game. It contains the list
+///of players and has a unique ID, as well as indicates which user is the game
+///administrator (by ID).
 class Game {
   int gameID;
   int adminID;
   List<LeaderboardItem> leaderboard;
   bool gameOver;
 
+  ///When calling this constructor, use the PlayerID of whoever is creating the
+  ///game (aID) and the new gameID from the database (gID).
   Game(int gID, int aID){
     gameID = gID;
     adminID = aID;
   }
 
+  ///This method sets the initial list of targets based on the available Players
+  ///in the database.
   void setTargets() {
     //get list of players from remote database
 
@@ -21,12 +28,17 @@ class Game {
     //set localPlayer.targetID to appropriate value
   }
 
+  ///This method updates the local list of other players in the game.
+  ///(Is this the leaderboard?)
   void getUsers() {
     //access remote database
 
     //update local database/list
   }
 
+  ///This method indicates that the game has started, and updates the localPlayer
+  ///variable, getting information from the database(should it need to? Local
+  ///player information can be created and stored locally.).
   //when adding a Player to a game: if(gameOver){return;}
   void startGame() {
     gameOver = false;// update database to show game in progress
@@ -35,9 +47,16 @@ class Game {
     Player localPlayer = new Player(/*localplayerinfo*/); //localPlayer should be a global variable, pending further implementation.
   }
 
+  ///This method retrieves the most up-to-date leaderboard information from the
+  ///database, sorted by score.
   List<LeaderboardItem> getLeaderboard() {
     leaderboard = new List<LeaderboardItem>();
     //get all players from database, organized by score
     return leaderboard;
   }
+
+  ///getter methods
+  int getGameID()=>gameID;
+  int getAdminID()=>adminID;
+  bool isGameOver()=>gameOver;
 }
--- lib/LeaderboardItem.dart
+++ lib/LeaderboardItem.dart
@@ -1,5 +1,8 @@
+///This class represents a single item on the game leaderboard, identifying
+///players by username, ranking them, and providing their points value.
 class LeaderboardItem{
   int position;
   String name;
   int points;
+  //bool isEliminated;
 }
